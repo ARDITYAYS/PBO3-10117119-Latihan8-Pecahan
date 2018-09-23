cac498b7b34158ff9e12eaf08d05cc86647e2180
ARDITYAYS <220499yogaputa@gmail.com>
2018-09-23 21:03:46 +0700

    first commit

---
+++ src/pbo310117119latihan8/PBO310117119Latihan8.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo310117119latihan8;
+
+/**
+ *
+ *@author  
+ * Nama      : Arditya Yogaputra S
+ * Kelas     : IF-3 / PBO-3
+ * Nim       : 10117104
+ * Deskripsi : Program ini berisi fungsi Pecahan dalam bentuk desimal 
+ *
+ */
+public class PBO310117119Latihan8 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float : " + f);
+        System.out.println("Double : " + d);
+    }
+    
+}
